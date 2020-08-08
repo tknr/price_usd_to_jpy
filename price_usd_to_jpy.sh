b3d724ddee55d7c6120575246b953ab288218db2
tknr <geishagirlskoume@gmail.com>
2020-08-08 16:09:41 +0900

    2020年  8月  8日 土曜日 16:09:41 JST

---
+++ price_usd_to_jpy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+MAILADDRESS_TO=example@example.com
+PRICE_USD=100
+
+if [ $# -lt 1 ]; then
+  echo "usage:${BASH_SOURCE[0]##*/} [us dollar price] [mailaddress]"
+  exit 1
+fi
+
+MAILADDRESS_TO=$2
+PRICE_USD=$1
+
+JPY=`wget -q -O - 'https://api.exchangeratesapi.io/latest?base=USD&symbols=JPY'`
+RATE=`echo $JPY | jq -r '.rates["JPY"]'`
+PRICE_JPY=$PRICE_USD*$RATE
+PRICE=`echo $PRICE_JPY|bc -l`
+
+if [ $# -eq 1 ]; then
+  printf '%.0f yen' $PRICE
+  exit 0
+fi
+
+BODY="$PRICE_USD USD is $PRICE JPY ."
+echo "rate : $RATE"$'\n'"$BODY" | mail -s "[price report] $BODY" $MAILADDRESS_TO
